6204441aa3743772c901d195cd247f28deb5c4a7
Boris A. Burkov <vasjaforutube@gmail.com>
2018-08-07 17:52:37 +0100

    Topics enum, hashmap of workers.

--- src/main/java/org/rnacentral/Job.java
+++ src/main/java/org/rnacentral/Job.java
@@ -2,11 +2,13 @@ package org.rnacentral;
 
 import java.util.ArrayList;
 
+import org.rnacentral.Mapper.Topic;
+
 public class Job {
     String id;
-    ArrayList<String> databases;
+    ArrayList<Topic> databases;
 
-    public Job(String id, ArrayList<String> databases) {
+    public Job(String id, ArrayList<Topic> databases) {
         this.id = id;
         this.databases = databases;
     }
--- src/main/java/org/rnacentral/Mapper.java
+++ src/main/java/org/rnacentral/Mapper.java
@@ -1,32 +1,49 @@
 package org.rnacentral;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
 import org.rnacentral.Job;
 
 public class Mapper extends AbstractActor {
+    // List of all topics
+    public enum Topic {
+        tRNA,
+        rRNA,
+        miRBase
+    }
+
     private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
     // Map of workers to the original actors requesting the calculation
-    private Map<String, ActorRef> pendingWorkers = new HashMap<>();
+    private Map<Topic, ActorRef> workers = new HashMap<>();
+
+    public Mapper() {
+        for (Topic topic : Topic.values()) {
+            ActorRef worker = getContext().getSystem().actorOf(Props.create(Worker.class), topic.toString());
+            this.workers.put(topic, worker);
+        }
+    }
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
-                .match(Job.class, s -> {
-                    log.info("Received Job: {}", s);
+                .match(Job.class, job -> {
+                    log.info("Received Job: {}", job);
 
-                    for (String topic : job.databases) {
-                        topic
+                    for (Topic topic : job.databases) {
+                        ActorRef worker = workers.get(topic);
                     }
 
                     // We delegate the job of running NHMMER to a worker, passing the
                     // job as a constructor argument to the actor.
                     ActorRef worker = getContext().actorOf(Props.create(Worker.class));
-                    pendingWorkers.put(, worker);
                 })
                 .matchAny(o -> log.info("received unknown message"))
                 .build();
